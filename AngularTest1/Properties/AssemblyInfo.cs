f0c3a1517532c982aa9d9ba9ee00e4c2d96a149c
sebastian.badea <sebastian.badea@gmail.com>
2016-10-11 14:17:57 +0300

    initial solution

---
+++ AngularTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AngularTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AngularTest1")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8d3c41ff-922e-4212-9391-85b2581425c0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AngularTest1/scripts/proj/index.js
@@ -0,0 +1,21 @@
+﻿/// <reference path="../libs/angular.min.js" />
+
+(function () {
+    //define a module; the name of the module can be specified in 'ng-app' directive
+    var app = angular.module("githubViewer", []);
+
+    //$scope is where the properties can be added
+    //$http is used to make requests
+    var MainController = function ($scope, $http) {
+        var onUserComplete = function (response) {
+            $scope.user = response.data;
+        }
+
+        $scope.message = "some text";
+
+        $http.get("https://api.github.com/users/sebastianbadea")
+             .then(onUserComplete);
+    };
+    //add controller to module; the name of the controller can be specified in 'ng-controller' directive
+    app.controller("MainController", ["$scope", "$http", MainController]);
+}());
