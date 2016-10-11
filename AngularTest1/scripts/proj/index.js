f418d7044cc956cb9c2ed2ad2832c788df63db0b
sebastian.badea <sebastian.badea@gmail.com>
2016-10-11 16:36:34 +0300

    adding directives

--- AngularTest1/scripts/proj/index.js
+++ AngularTest1/scripts/proj/index.js
@@ -9,12 +9,28 @@
     var MainController = function ($scope, $http) {
         var onUserComplete = function (response) {
             $scope.user = response.data;
-        }
+            $http.get($scope.user.repos_url)
+                 .then(onReposComplete, onError);
+        };
 
-        $scope.message = "some text";
+        var onReposComplete = function (response) {
+            $scope.repos = response.data;
+        };
 
-        $http.get("https://api.github.com/users/sebastianbadea")
-             .then(onUserComplete);
+        var onError = function (reason) {
+            $scope.error = "Could not fetch the data because" + reason;
+            $scope.user = undefined;
+        };
+
+        $scope.message = "github viewer";
+
+        $scope.search = function () {
+            $http.get("https://api.github.com/users/" + $scope.username)
+                 .then(onUserComplete, onError);
+        };
+        //the 'sortOrder' field is used for sorting an 'ng-repeat'; the '-' means that is sorting in descending order
+        $scope.sortOrder = "-language";
+        $scope.username = "sebastianbadea";
     };
     //add controller to module; the name of the controller can be specified in 'ng-controller' directive
     app.controller("MainController", ["$scope", "$http", MainController]);
