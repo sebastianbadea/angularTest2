254d59a26c0d1f4daded76bcb89ad89326750978
sebastian.badea <sebastian.badea@gmail.com>
2016-10-17 17:53:01 +0300

    add controllers and the first service

---
+++ EventReq/scripts/proj/controllers/editEventController.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="../../libs/angular.js" />
+'use strict';
+var editEventController = function ($scope) {
+    $scope.save = function (event, form) {
+        //when a validation is made angular put attributes on fields(ng-invalid ng-invalid-required, etc) and they can be defined in css to display invalid fields(change backcolor, etc)
+        if (form.$valid) {//property provided by angular to verify if a form is valid
+
+            alert('saving model');
+        }
+    }
+};
+
+var app = angular.module('eventsApp');
+app.controller('editEventController', ['$scope', editEventController]);
---
+++ EventReq/scripts/proj/services/eventDataService.js
@@ -0,0 +1,41 @@
+﻿eventsApp.factory('eventData', function () {
+    return {
+        event: {
+            name: 'Angular boot camp',
+            date: '1/1/2013',
+            time: '10:30 am',
+            imageUrl: '../content/images/banner-angularjs.jpg',
+            location: {
+                address: 'some address',
+                city: 'Bucharest',
+                province: 'Pipera'
+            },
+            sessions: [
+                {
+                    name: 'Dirrectives Masterclass',
+                    creatorName: 'John Smith',
+                    duration: '1 hr',
+                    level: 'Advanced',
+                    abstract: 'In this session whatever',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Scopes for fun and profit',
+                    creatorName: 'John Smith jr',
+                    duration: '30 min',
+                    level: 'Introductory',
+                    abstract: 'In this session whatever',
+                    upVoteCount: 0
+                },
+                {
+                    name: 'Well behaved controllers',
+                    creatorName: 'John Smith',
+                    duration: '2 hr',
+                    level: 'Medium',
+                    abstract: 'In this session whatever',
+                    upVoteCount: 0
+                }
+            ]
+        }
+    };
+});
